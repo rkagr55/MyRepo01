1aeb7baca4f926ff2dacfa7e0d2973db32c99b11
rkagr55 <85919096+rkagr55@users.noreply.github.com>
2022-01-06 20:22:12 +0530

    Update BaseClass.java

--- Artifact001/src/test/java/main/BaseClass.java
+++ Artifact001/src/test/java/main/BaseClass.java
@@ -46,8 +46,7 @@ public class BaseClass {
 	      	options.addArguments("--no-sandbox");
         	options.addArguments("--disable-dev-shm-usage");
         	options.addArguments("--headless");
-	        options.addArguments("--screenshot");
-	        //options.addArguments("window-size=1400,800");
+	        options.addArguments("window-size=1400,800");
     	  	driver = new ChromeDriver(options);
       }
       driver.manage().window().maximize(); 
